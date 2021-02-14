e216cb3bc30786507ddf0bd6f25cd7695c9c0526
Hannah Hong <hannahhong@Hannahs-MacBook-Pro-3.local>
2021-02-14 15:29:51 -0800

    Add event handlers for button click

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Music Converter incorporated by Noodley and Ghey' });
+  res.render('index', 
+  { title: 'Music Converter incorporated by Noodley and Ghey',
+    appleMusicSignIn: "Sign in to Apple Music",
+    spotifySignIn: 'Sign in to Spotify' });
+});
+
+router.post('/AMSignIn',  function(req, res) {
+  res.render('index', 
+  { title: 'Music Converter incorporated by Noodley and Ghey',
+    appleMusicSignIn: 'signed in!',
+    spotifySignIn: 'Sign in to Spotify' });
 });
 
 module.exports = router;
